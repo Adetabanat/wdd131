04364f3d6800e07664dbe9cb6953378917af312d
AdongoNat <danieladongonat@gmail.com>
2025-01-18 21:31:54 +0000

    hambuger

--- scripts/temple.js
+++ scripts/temple.js
@@ -4,25 +4,25 @@ if (currentYearElement) {
     currentYearElement.textContent = currentYear;
 }
 
-// Get the last modified date of the document and set it in the footer
+
 const lastModifiedElement = document.getElementById('lastModified');
 if (lastModifiedElement) {
     lastModifiedElement.textContent = `Last Modified: ${document.lastModified}`;
 }
   
-  // Hamburger menu functionality
+ 
   const hamburger = document.querySelector("#hamburger");
   const navMenu = document.querySelector("nav ul");
   
   hamburger.addEventListener("click", () => {
-    // Toggle the visibility of the navigation menu
+    
     navMenu.classList.toggle("active");
     
-    // Change the hamburger icon to an 'X' when active
+   
     if (navMenu.classList.contains("active")) {
-      hamburger.textContent = "X"; // Change the symbol to 'X'
+      hamburger.textContent = "X"; 
     } else {
-      hamburger.textContent = "="; // Change back to the hamburger symbol
+      hamburger.textContent = "="; 
     }
   });
   
